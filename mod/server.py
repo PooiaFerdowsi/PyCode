a698700404741d009195c0b4ff33fec24bfcf655
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-02-27 12:30:14 +0330

    Create server.py

---
+++ mod/server.py
@@ -0,0 +1,25 @@
+import http.server
+def start(address:tuple=("localhost",88))->http.server.HTTPServer:
+  """ This is user defined Request Handler function and may be Override """
+	httpd = http.server.HTTPServer(
+		address,
+		http.server.BaseHTTPRequestHandler)
+	return httpd
+
+def startBase(address:tuple=("localhost",80))->http.server.HTTPServer:
+	httpd = http.server.HTTPServer(
+		address,
+		http.server.BaseHTTPRequestHandler)
+	return httpd
+
+def startSimple(address:tuple=("localhost",88))->http.server.HTTPServer:
+	httpd = http.server.HTTPServer(
+		address,
+		http.server.SimpleHTTPRequestHandler)
+	return httpd
+
+def startCGI(address:tuple=("localhost",88))->http.server.HTTPServer:
+	httpd = http.server.HTTPServer(
+		address,
+		http.server.BaseHTTPRequestHandler)
+	return httpd
