d770aeedf0bb34e28578417d0eae6756cb61d9d3
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-03-06 11:22:39 +0330

    Update PyCode.py

--- PyCode.py
+++ PyCode.py
@@ -1,4 +1,27 @@
 """ Main Code """
 import mod
+from typing import (
+  Any
+)
 """ Create Server """
 mod.start().serve_forever() # method caching 
+
+def function(*,func,
+             **kwd
+            ) -> Any:
+   return func
+  
+function(func=lambda :"HelloWorld!")()
+function(func=lambda x,y:x.split("y")(x="Hello , World",y=",")
+f = function(func=lambda :"HelloWorld!")
+f()
+del f
+
+def function(func,
+             **kwd
+             ,/
+            ) -> Any:
+   return func()
+  
+function(lambda :"HelloWorld")
+  
