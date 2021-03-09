32c6ca0f4814805ac532cfcea644d032c4e21df4
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-03-09 20:38:20 +0330

    Update PyCode.py

--- PyCode.py
+++ PyCode.py
@@ -41,7 +41,6 @@ def function(*params):
  
 
          
-         
 def tuple_args(*args):
          return type(args) # tuple
          
@@ -56,3 +55,47 @@ raise UsrErr
 # create a new  UsrErr Exception &  The UsrErr Exception is a BaseException child class
 #good comment
          
+         
+# walrus from Russian (:) # The Animal
+         
+try:
+         param = "Hello World If I"
+         print(f"Hello {x:=len(param)}")
+except:
+         import http.server
+         class Handlerr(http.server.BaseHTTPRequestHandler):
+                def do_get(this):
+                    this.send_response(200)
+                    this.send_header("Content-type: text/html")
+                    this.end_headers()
+                    this.wfile.write("<h1> You have a Exception You can not use of walrus in f-str ( formatted string ) </h1>")
+        http.server.HTTPServer(("localhost",373),Handlerr);
+        exit()
+        quit()
+
+def HelloWorld(f): # function is not-std ( Pascal-case )
+         return "Hello World"
+         
+@HelloWorld
+def printing():
+         return """
+         if I don any work !!!!
+         I only !!!!
+         print 
+         "Hello World"::::->()(!!!!
+         Good{}
+         bye[]
+         bytes[];
+         *ptr;
+         **dptr;
+         <Generic>
+         (param) -> {code} lambda java
+         lambda param:code lambda python
+         #include "" import C
+         String...name == *param:str # True or 1 or true
+         """
+         
+print(printing())
+         # Hello World
+         
+ 
